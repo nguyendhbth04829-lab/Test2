c1527f236efd1be5c1da4d034a2b093557dafdc9
Nguyen <emailcuaban@gmail.com>
2026-04-19 18:01:13 +0700

    Break test tinhtongmang()

--- src/main/java/vn/fpoly/demo22/methods/MyUtil.java
+++ src/main/java/vn/fpoly/demo22/methods/MyUtil.java
@@ -1,4 +1,17 @@
 package vn.fpoly.demo22.methods;
 
 public class MyUtil {
+    public int tinhtongmang(int arr[]){
+        if (arr==null||arr.length==0){
+            throw new IllegalArgumentException("mang khong hop le");
+        }
+        int tong = 0;
+        for (int x:arr){
+            if (x<1||x>1000){
+                throw new IllegalArgumentException("phan tu phai lon hon 1 va be hon 1000");
+            }
+            tong += x+1;
+        }
+        return tong;
+    }
 }
